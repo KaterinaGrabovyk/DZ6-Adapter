0c68a29ba7a3cdbf424282d6998cac2a4625d632
KatyaGrabovyk <katya.grabovik@ukr.net>
2023-12-12 09:32:52 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DZ
+{
+    public interface IQuest
+    {
+        string GetResult();
+    }
+
+
+    class Adapteer
+    {
+        public string GetNewResult()
+        {
+            return "Нове завдання";
+        }
+    }
+
+    class Adapter : IQuest
+    {
+        private readonly Adapteer adap;
+
+        public Adapter(Adapteer adaptee)
+        {
+            this.adap = adaptee;
+        }
+
+        public string GetResult()
+        {
+            return $"Це '{this.adap.GetNewResult()}'";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapteer adap = new Adapteer();
+            IQuest q = new Adapter(adap);
+
+
+            Console.WriteLine(q.GetResult());
+        }
+    }
+}
